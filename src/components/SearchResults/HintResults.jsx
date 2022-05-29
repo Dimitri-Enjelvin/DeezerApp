f3b663ba5ce30943b55224cd912ac7905c8a968f
Dimitri Enjelvin <dimitri.enjelvin@gmail.com>
2022-05-29 23:39:13 +0200

    adding search tracks feature + small changes

--- src/components/SearchResults/HintResults.jsx
+++ src/components/SearchResults/HintResults.jsx
@@ -5,42 +5,13 @@ import { Link } from "react-router-dom";
 
 import ItemResult from "./ItemResult";
 
-const HintResults = ({ artistsFound, albumsFound, songsFound }) => {
+const HintResults = ({ songsFound }) => {
+
+  // if(artistsFound === undefined && albumsFound === undefined && songsFound === undefined) return null
+  
   return (
     <div id="hint-container">
       <div>
-        <section>
-          <h3 className="title-section">Artists</h3>
-          {artistsFound.data.length > 0 ? (
-            artistsFound.data.map((artist) => (
-              <Link to={`/artist/${artist.id}`}>
-                <ItemResult
-                  key={artist.id}
-                  text={artist.name}
-                  pic={artist.picture_small}
-                />
-              </Link>
-            ))
-          ) : (
-            <div className="error">No Matches Were Found</div>
-          )}
-        </section>
-        <section>
-          <h3 className="title-section">Albums</h3>
-          {albumsFound.data.length > 0 ? (
-            albumsFound.data.map((album) => (
-              <Link to={`/album/${album.id}`}>
-                <ItemResult
-                  key={album.id}
-                  text={album.title}
-                  pic={album.cover_small}
-                />
-              </Link>
-            ))
-          ) : (
-            <div className="error">No Matches Were Found</div>
-          )}
-        </section>
         <section>
           <h3 className="title-section">Songs</h3>
           {songsFound.data.length > 0 ? (
