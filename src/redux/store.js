2b8bbb01e1a5053b04abcd0f79138fdb392c405a
Dimitri Enjelvin <dimitri.enjelvin@gmail.com>
2022-05-29 23:37:17 +0200

    handle redux issue

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,20 +1,17 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-// Or from '@reduxjs/toolkit/query/react'
+import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers } from 'redux'
 import { setupListeners } from '@reduxjs/toolkit/query'
-import { deezerApi } from '../api'
 import { favoriteSlice }  from './slice/favoriteSlice'
 
-export const store = configureStore({
-  reducer: {
-    // Add the generated reducer as a specific top-level slice
-    [deezerApi.reducerPath]: deezerApi.reducer,
-  },
-  // Adding the api middleware enables caching, invalidation, polling,
-  // and other useful features of `rtk-query`.
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(deezerApi.middleware),
+const reducers = combineReducers({
+  favorites: favoriteSlice.reducer,
 })
 
-// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
-// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+export const store = configureStore({
+  reducer: reducers,
+
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware().concat(favoriteSlice.reducer.middleware),
+})
+
 setupListeners(store.dispatch)
